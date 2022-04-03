23816e17250cf1a8c64cfab3ce9a4094edf755b7
Khiem Pham <pk.22092000@gmail.com>
2022-04-03 19:28:04 +0700

    a

--- fe/src/screens/SearchScreen.js
+++ fe/src/screens/SearchScreen.js
@@ -31,7 +31,7 @@ export default function SearchScreen(props) {
   console.log(animals)
 
   return (
-    <div className='container'>
+    <div className='container search-wrap'>
       <div>Kết quả tìm kiếm từ:  {props.id}</div>
       <div className='row row-cols-2'>
       {
