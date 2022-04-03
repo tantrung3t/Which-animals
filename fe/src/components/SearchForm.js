36177c4b14a2a6136fe6124bf7a0bcb501ec6691
Khiem Pham <pk.22092000@gmail.com>
2022-04-03 19:22:55 +0700

    a

--- fe/src/components/SearchForm.js
+++ fe/src/components/SearchForm.js
@@ -4,8 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 export default function SearchForm() {
+    const handleSearch = () => {
+        console.log("search")
+    }
 
     return (
+
         <div className="header__search">
             <form className="header__search-form">
                 <i className="ti-search header__search-icon"></i>
@@ -14,7 +18,7 @@ export default function SearchForm() {
                     tp="text"
                     placeholder="Nhập tên con vât muốn tìm"
                 />
-                <button className="btn-search"><FontAwesomeIcon icon={ faMagnifyingGlass } /></button>
+                <button className="btn-search"><FontAwesomeIcon icon={ faMagnifyingGlass } onClick={handleSearch}/></button>
             </form>
         </div>
     );
