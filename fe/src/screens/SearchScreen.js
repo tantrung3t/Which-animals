045289c4a3bf990629a04d28d7f3c16112d300bc
Khiem Pham <pk.22092000@gmail.com>
2022-04-03 19:19:23 +0700

    search sreen

--- fe/src/screens/SearchScreen.js
+++ fe/src/screens/SearchScreen.js
@@ -1,8 +1,51 @@
+import React, { useEffect, useState } from 'react'
 
-export default function SearchScreen(props){
-    return(
-        <div>
-            Danh sach tim kiem tu {props.id}
-        </div>
-    )
+export default function SearchScreen(props) {
+  const [animals, setAnimals] = useState([])
+  const data = [
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Mimus_polyglottus1_cropped.png',
+    name: "chim",
+    nameEng: "bird"
+  },
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Mimus_polyglottus1_cropped.png',
+    name: "chim",
+    nameEng: "bird"
+  },
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Mimus_polyglottus1_cropped.png',
+    name: "chim",
+    nameEng: "bird"
+  },
+  {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Mimus_polyglottus1_cropped.png',
+    name: "chim",
+    nameEng: "bird"
+  }
+]
+
+  useEffect(() => {
+    setAnimals(data)
+  }, [])
+  console.log(animals)
+
+  return (
+    <div className='container'>
+      <div>Kết quả tìm kiếm từ:  {props.id}</div>
+      <div className='row row-cols-2'>
+      {
+          animals.map((animal, index) => 
+            <div className='search-result' key={index}>
+              <img src={animal.img} alt='img' className='search-img'/>
+              <div>
+                <div className='search-name'> {animal.name} </div>
+                <div className='search-name'> {animal.nameEng} </div>
+              </div>
+            </div>
+          )
+        }
+      </div>
+    </div>
+  )
 }
